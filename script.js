4875a6e241b4a343fd9872d097ade660d8688fb8
Kevin Toro <ktoro1251@gmail.com>
2019-05-03 12:49:43 -0400

    substituted with template strings

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 $(document).ready(function(){
-    console.log("i am ready");
     
-    
-    
-    //javascript, jQuery
-    //use the following two lines to print a json response
-    //jquery handler on go button
-    //let api_key = "qIp4FtndglssrZT2Ce1cBIL2GhyWt38q";
+
     $("#findBtn").click(function(e) {
-        var input = $("input:text").val();
         e.preventDefault();
-        console.log(input);
+        let url = "http://api.giphy.com/v1/gifs/search?q=";
+        let query = $("input:text").val();
+        let key = "qIp4FtndglssrZT2Ce1cBIL2GhyWt38q";
+        let limit = 9;
+        let xhr = $.get(`${url}${query}${"&api_key="}${key}${"&limit="}${limit}`);
         
-        var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+input+"&api_key=qIp4FtndglssrZT2Ce1cBIL2GhyWt38q&limit=6");
+        //let xhr = $.get("http://api.giphy.com/v1/gifs/search?q=birds&api_key=qIp4FtndglssrZT2Ce1cBIL2GhyWt38q&limit=6");
+        
         xhr.done(function(response) { 
             console.log("success got data", response); 
-
-            var gifs = response.data
+            let gifs = response.data
 
             for(property in gifs){
-                $('#results').append("<img src='"+gifs[property].images.original.url+"' style='height:350px; width:350px;'/>");
+                $('#results').append(`<img src= ${gifs[property].images.original.url} style='height:350px; width:350px;'/>`);
             }
         });
-        //loop through data and print
+        
     });
 });
